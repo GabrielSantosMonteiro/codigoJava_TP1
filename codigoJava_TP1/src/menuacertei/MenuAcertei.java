3925d8756e455ec27d7d13ca143df8eeb120f1ce
GabrielSantosMonteiro <111158876+GabrielSantosMonteiro@users.noreply.github.com>
2023-05-02 21:48:46 -0300

    Add files via upload

---
+++ codigoJava_TP1/src/menuacertei/MenuAcertei.java
@@ -0,0 +1,203 @@
+package menuacertei;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MenuAcertei {
+
+    public static void main(String[] args) {
+    	
+        ArrayList<String> questoes = new ArrayList<String>();
+        ArrayList<String> editais = new ArrayList<String>();
+        ArrayList<String> ids_Edital = new ArrayList<String>();
+        ArrayList<String> datasinicio = new ArrayList<String>();
+        ArrayList<String> datasfinais = new ArrayList<String>();
+        ArrayList<String> vagas = new ArrayList<String>();
+        ArrayList<String> bancas = new ArrayList<String>();
+        ArrayList<String> requisitos = new ArrayList<String>();
+        
+        try (Scanner scanner = new Scanner(System.in)) {
+			int opcao = 0;
+
+			do {
+			    System.out.println("=========== MENU ===========");
+			    System.out.println("1 - Cadastrar Questão");
+			    System.out.println("2 - Excluir Questão");
+			    System.out.println("3 - Consultar questões cadastradas");
+			    System.out.println("4 - Cadastrar Edital");
+			    System.out.println("5 - Excluir Edital");
+			    System.out.println("6 - Exibir Editais Cadastrados");
+			    System.out.println("7 - Encerrar");
+			    System.out.println("============================");
+			    System.out.println("Digite a opção desejada:  ");
+			    opcao = scanner.nextInt();
+			    scanner.nextLine();																									
+
+			    switch (opcao) {
+			        case 1:
+			            System.out.println("Digite a questão a ser cadastrada:");
+			            String questao = scanner.nextLine();
+			            questoes.add(questao);
+			            System.out.println("Questão cadastrada com sucesso!");
+			            break;
+			            
+			        case 2:{
+			            System.out.println("As questões são feitas cadastradas em ordem, deseja ver as questões cadastradas em ordem?");
+			            System.out.println("1- Sim 2 - Não");
+			            int escolha = scanner.nextInt();
+			            
+			            if (escolha == 1) {
+			            	System.out.println("As questões cadastradas são:");
+				            System.out.println("================================");
+				            for (int u = 0; u < questoes.size(); u++) {
+			                    System.out.println(u + "-" + questoes.get(u));
+			                }
+				            System.out.println("================================");
+				            
+				            System.out.println("Digite o índice da questão que deseja excluir");
+				            int indice = scanner.nextInt();
+				            scanner.nextLine();
+				            if (indice >= 0 && indice < questoes.size()) {
+				                questoes.remove(indice);
+				                System.out.println("Questão excluída com sucesso!");
+				            break;}}
+			            
+			            if (escolha == 2) {
+			    			System.out.println("Digite o índice da questão que deseja excluir");
+				            int indice = scanner.nextInt();
+				            scanner.nextLine();
+				            if (indice >= 0 && indice < questoes.size()) {
+				                questoes.remove(indice);
+				                System.out.println("Questão excluída com sucesso!");
+				            }} else {
+				                System.out.println("Índice inválido!");
+				            }}
+				            break;
+			            
+			        case 3:{
+			        	if (questoes.isEmpty()) {
+			        		System.out.println("Não há questões cadastradas!");
+			        		break;
+			        	} else {
+			        		System.out.println("As questões cadastradas são:");
+				            System.out.println("================================");
+				            for (String q : questoes) {
+				                System.out.println(q);
+				            }
+				            System.out.println("================================");
+				            break;
+			        	}}
+			            
+			            
+			        case 4:
+			        	
+			        	System.out.println("Nome:");
+			            String nomeEdital = scanner.nextLine();
+			            editais.add(nomeEdital);
+			            
+			            System.out.println("ID do Edital:");
+			            String ID_Edital = scanner.nextLine();
+			            ids_Edital.add(ID_Edital);
+			            
+			            System.out.println("Data início:");
+			            String dataInicio = scanner.nextLine();
+			            datasinicio.add(dataInicio);
+			            
+			            System.out.println("Data final:");
+			            String dataFinal = scanner.nextLine();
+			            datasfinais.add(dataFinal);
+			            
+			            System.out.println("Número de vagas:");
+			            String vaga = scanner.nextLine();
+			            vagas.add(vaga);
+			            
+			            System.out.println("Banca:");
+			            String banca = scanner.nextLine();
+			            bancas.add(banca);
+			            
+			            System.out.println("Requisitos da vaga:");
+			            String requisito = scanner.nextLine();
+			            requisitos.add(requisito);
+			            
+			            System.out.println("\nEdital cadastrado com sucesso!");
+			            
+			            break;
+				            
+			        case 5: {
+			            if (editais.isEmpty()) {
+			                System.out.println("Não há editais cadastrados!");
+			            } else {
+			                System.out.println("Os editais cadastrados são:");
+			                System.out.println("================================");
+			                for (int i = 0; i < editais.size(); i++) {
+			                    System.out.println(i + "-" + editais.get(i));
+			                }
+			                System.out.println("================================");
+			                System.out.println("Digite o índice do edital que deseja excluir");
+			                int indice = scanner.nextInt();
+			                scanner.nextLine();
+			                if (indice >= 0 && indice < editais.size()) {
+			                    
+			                	editais.remove(indice);
+			                    
+			                    ids_Edital.remove(indice);
+			                    
+			                    datasinicio.remove(indice);
+			                    
+			                    datasfinais.remove(indice);
+			                    
+			                    vagas.remove(indice);
+			                    
+			                    bancas.remove(indice);
+			                    
+			                    requisitos.remove(indice);
+			                    
+			                    System.out.println("Edital excluído com sucesso!");
+			                } else {
+			                    System.out.println("Índice inválido!");
+			                }
+			            }
+			            break;
+			        }
+
+			        case 6:
+			            if (editais.isEmpty()) {
+			                System.out.println("Não há editais cadastrados!");
+			            } else {
+			                System.out.println("Os editais cadastrados são:");
+			                System.out.println("================================");
+			                for (int i = 0; i < editais.size(); i++) {
+			                	
+			                    
+			                	System.out.println(ids_Edital.get(i) + "-" + editais.get(i));
+			                    
+			                	System.out.println("Data de início: " + datasinicio.get(i));
+			                    
+			                	System.out.println("Data de término: " + datasfinais.get(i));
+			                    
+			                	System.out.println("Número de vagas: " + vagas.get(i));
+			                    
+			                	System.out.println("Banca examinadora: " + bancas.get(i));
+			                    
+			                	System.out.println("Requisitos: " + requisitos.get(i));
+			                    
+			                	System.out.println("--------------------------------");
+			                }
+			                System.out.println("================================");
+			            }
+			            break;
+
+			        case 7:
+			            System.out.println("Encerrando sistema...");
+			            System.out.println("Até a próxima!");
+			            break;
+			        default:
+			            System.out.println("Opção inválida!");
+			    }
+
+			    System.out.println();
+
+			} while (opcao != 7);
+			scanner.close();
+		}
+    }
+}
---
+++ codigoJava_TP1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gabri
+ *
+ */
+module codigoJava_TP1 {
+}

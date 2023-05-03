49fe3a810004509f4c27bd5478297eb17cf8df59
GabrielSantosMonteiro <111158876+GabrielSantosMonteiro@users.noreply.github.com>
2023-05-02 22:43:05 -0300

    Update MenuAcertei.java

--- codigoJava_TP1/src/menuacertei/MenuAcertei.java
+++ codigoJava_TP1/src/menuacertei/MenuAcertei.java
@@ -14,6 +14,8 @@ public class MenuAcertei {
         ArrayList<String> vagas = new ArrayList<String>();
         ArrayList<String> bancas = new ArrayList<String>();
         ArrayList<String> requisitos = new ArrayList<String>();
+        ArrayList<String> materias = new ArrayList<String>();
+        ArrayList<Integer> questoesPorMateria = new ArrayList<Integer>();
         
         try (Scanner scanner = new Scanner(System.in)) {
 			int opcao = 0;
@@ -23,10 +25,11 @@ public class MenuAcertei {
 			    System.out.println("1 - Cadastrar Questão");
 			    System.out.println("2 - Excluir Questão");
 			    System.out.println("3 - Consultar questões cadastradas");
-			    System.out.println("4 - Cadastrar Edital");
-			    System.out.println("5 - Excluir Edital");
-			    System.out.println("6 - Exibir Editais Cadastrados");
-			    System.out.println("7 - Encerrar");
+			    System.out.println("4 - Listar por matéria");
+			    System.out.println("5 - Cadastrar Edital");
+			    System.out.println("6 - Excluir Edital");
+			    System.out.println("7 - Exibir Editais Cadastrados");
+			    System.out.println("8 - Encerrar");
 			    System.out.println("============================");
 			    System.out.println("Digite a opção desejada:  ");
 			    opcao = scanner.nextInt();
@@ -36,6 +39,17 @@ public class MenuAcertei {
 			        case 1:
 			            System.out.println("Digite a questão a ser cadastrada:");
 			            String questao = scanner.nextLine();
+
+			            System.out.println("Digite a matéria da questão:");
+			            String materia = scanner.nextLine();
+			            int indexMateria = materias.indexOf(materia);
+			            if(indexMateria == -1){
+			                materias.add(materia);
+			                questoesPorMateria.add(1);
+			            } else {
+			                questoesPorMateria.set(indexMateria, questoesPorMateria.get(indexMateria) + 1);
+			            }
+			            
 			            questoes.add(questao);
 			            System.out.println("Questão cadastrada com sucesso!");
 			            break;
@@ -87,8 +101,26 @@ public class MenuAcertei {
 				            break;
 			        	}}
 			            
-			            
 			        case 4:
+			        	if (materias.isEmpty()) {
+			        	System.out.println("Não há matérias cadastradas!");
+			        	break;
+			        	}
+			        	for (int i = 0; i < materias.size(); i++) {
+			        	String materia2 = materias.get(i);
+			        	
+			        	System.out.printf("Matéria: " + materia2);
+			        	for (int j = 0; j < questoes.size(); j++) {
+			        	if (materia2.equals(materias.get(j))) {
+			        		
+			        	System.out.printf("\n Questão %d: %s\n", j, questoes.get(j));
+			        	}
+			        	}
+			        	System.out.println("================================");
+			        	}
+			        	break;
+			        	
+			        case 5:
 			        	
 			        	System.out.println("Nome:");
 			            String nomeEdital = scanner.nextLine();
@@ -122,7 +154,7 @@ public class MenuAcertei {
 			            
 			            break;
 				            
-			        case 5: {
+			        case 6: {
 			            if (editais.isEmpty()) {
 			                System.out.println("Não há editais cadastrados!");
 			            } else {
@@ -159,7 +191,7 @@ public class MenuAcertei {
 			            break;
 			        }
 
-			        case 6:
+			        case 7:
 			            if (editais.isEmpty()) {
 			                System.out.println("Não há editais cadastrados!");
 			            } else {
@@ -185,8 +217,8 @@ public class MenuAcertei {
 			                System.out.println("================================");
 			            }
 			            break;
-
-			        case 7:
+			        	
+			        case 8:
 			            System.out.println("Encerrando sistema...");
 			            System.out.println("Até a próxima!");
 			            break;
@@ -196,7 +228,7 @@ public class MenuAcertei {
 
 			    System.out.println();
 
-			} while (opcao != 7);
+			} while (opcao != 8);
 			scanner.close();
 		}
     }
